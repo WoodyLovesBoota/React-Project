369825ab87386b0ad404e4cbaf7a8bcd9f18ba45
Woody <woodylovesboota@gmail.com>
2023-08-17 08:27:27 +0900

    add movie component

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,33 +1,5 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") return;
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-
-  return (
-    <div>
-      <h1>My ToDos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do.." />
-        <button>submit todo</button>
-      </form>
-      <hr />
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </div>
-  );
+  return null;
 }
 
 export default App;
