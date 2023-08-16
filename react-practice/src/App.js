a4a89a2acf07c03e3e6d9bc6bbbe79af241caba8
Woody <woodylovesboota@gmail.com>
2023-08-16 18:23:12 +0900

    add useEffect

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,11 +1,29 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyWord, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("only once");
+  };
+
+  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("search", keyWord);
+  }, [keyWord, counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!!</h1>
-      <Button text={"Continue"} />
+      <input type="text" placeholder="Search here..." onChange={onChange} value={keyWord} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
