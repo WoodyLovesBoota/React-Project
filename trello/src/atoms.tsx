ea5a693295332775c22b6830c62425f4a3ea3ca1
woody <woodylovesboota@gmail.com>
2023-09-06 10:10:15 +0900

    add localstorage section

--- trello/src/atoms.tsx
+++ trello/src/atoms.tsx
@@ -14,4 +14,15 @@ export const toDoState = atom<IToDoState>({
   default: {
     TrashLILI: [],
   },
+  effects: [
+    ({ setSelf, onSet }: any) => {
+      const savedValue = localStorage.getItem("toDo");
+      if (savedValue !== null) setSelf(JSON.parse(savedValue));
+      onSet((newValue: any, _: any, isReset: boolean) => {
+        isReset
+          ? localStorage.removeItem("toDo")
+          : localStorage.setItem("toDo", JSON.stringify(newValue));
+      });
+    },
+  ],
 });
