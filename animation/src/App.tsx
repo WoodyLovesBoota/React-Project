21c392858b14cf45d3e6b9f800a7f17f7c5998e9
woody <woodylovesboota@gmail.com>
2023-09-10 00:07:47 +0900

    add animation layer

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,124 +1,89 @@
 import styled from "styled-components";
 import {
+  AnimatePresence,
   motion,
   useMotionValue,
   useMotionValueEvent,
   useScroll,
   useTransform,
 } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, #e40195, #d0e);
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  height: 100px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  font-size: 28px;
+  align-items: center;
 `;
 
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.3);
-  border-radius: 40px;
+const Grid = styled.div`
+  width: 40vw;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
-  overflow: hidden;
 `;
 
 const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
+  background-color: #6e6ee3;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.5,
-    },
-  },
-};
-
-const circleVariants = {
-  start: { opacity: 0, y: 10 },
-  end: { opacity: 1, y: 0 },
-};
-
-const rotateVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
-};
-
-const boxVar = {
-  hover: {
-    scale: 1.5,
-    rotateZ: 90,
-  },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46, 203, 113)" },
-};
-
 function App() {
-  // const biggerBoxRef = useRef<HTMLDivElement>(null);
-  const x = useMotionValue(0);
-  // const scale = useTransform(x, [-300, 0, 300], [2, 1, 0.1]);
-  const rotateZ = useTransform(x, [-300, 300], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-300, 0, 300],
-    [
-      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
-      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
-      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-    ]
-  );
-  const { scrollY, scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 3]);
-  useMotionValueEvent(scrollY, "change", () => {
-    console.log(scrollY.get(), scrollYProgress.get());
-  });
-  useMotionValueEvent(x, "change", () => {
-    // console.log(scale.get());
-  });
+  const [id, setId] = useState<null | string>(null);
   return (
-    <Wrapper style={{ background: gradient }}>
-      {/* <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={1}
-          dragConstraints={biggerBoxRef}
-          variants={boxVar}
-          whileDrag={"drag"}
-          whileHover={"hover"}
-          whileTap={"click"}
-        ></Box>
-      </BiggerBox> */}
-      {/* <button onClick={() => x.set(200)}>click me</button> */}
-      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin></Box>
+    // <Wrapper onClick={toggleClicked}>
+    //   <Box>{!clicked && <Circle layoutId="circle"></Circle>}</Box>
+    //   <Box>{clicked && <Circle layoutId="circle"></Circle>}</Box>
+    // </Wrapper>
+    <Wrapper>
+      <Grid>
+        {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id && (
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
+            animate={{ backgroundColor: "rgba(0,0,0,0.6)" }}
+            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
+          >
+            <Box style={{ width: 400, height: 400 }} layoutId={id} />
+          </Overlay>
+        )}
+      </AnimatePresence>
     </Wrapper>
   );
 }
