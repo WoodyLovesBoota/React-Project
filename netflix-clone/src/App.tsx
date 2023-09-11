b8f8b639bc8e40bd349b29c85dbae7b90b2a7473
woody <woodylovesboota@gmail.com>
2023-09-11 13:20:51 +0900

    add project setup

---
+++ netflix-clone/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+import Header from "./Components/Header";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ netflix-clone/src/Routes/Home.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: black;
+  height: 200vh;
+`;
+
+const Home = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default Home;
